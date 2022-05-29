08015c50aac921d6f1fc3bd767beb60b77e4a21a
halllllll <h4l@yahoo.ne.jp>
2022-05-29 14:35:18 +0900

    get, await, async, script.google.run...

--- yo.js
+++ yo.js
@@ -1,5 +1,5 @@
 const ss = SpreadsheetApp.getActive();
-function myFunction() {
+function showThisURL() {
   console.log(ss.getUrl())
 }
 
@@ -12,4 +12,21 @@ function doGet(){
 function include(filename) {
     return HtmlService.createHtmlOutputFromFile(filename)
         .getContent();
-  }
+}
+
+/**
+ * Vueのgoogle.script.runから呼ばれる
+ * とりあえずsheetの全データを返す（二次元配列だがJsonとして返す）
+ */
+function getAllData(){
+    const sheet = ss.getActiveSheet();
+    const values = sheet.getDataRange().getValues();
+    const header = values.splice(0, 1)[0];
+    return values.map(function(row) {
+        let obj = {};
+        row.map((item, index) => {
+          obj[String(header[index])] = String(item);
+        });
+        return obj;
+      });
+}
