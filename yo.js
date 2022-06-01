a041cc2dc39af3acd1ea803fcf19cce5d3078db2
halllllll <h4l@yahoo.ne.jp>
2022-06-01 21:58:34 +0900

    refactor some methods(front-side, appsscript-side)

--- yo.js
+++ yo.js
@@ -40,24 +40,36 @@ function getAllData(header){
  * ワードが含まれる行を取得したい
  */
 function search(header, words){
-  // とくにjsonとか考えなくても文tableHeader2字列のまま取得できた 配列も同じ
-  const searchWords = `(${words.trim().replaceAll(/(　| |\\|\|)+/g, " ").split(" ").join("|")})`;
-  console.log(`search target words: ${searchWords}`);
-  // 検索対象はとりあえずタイトルだけ
-  const titleRange = sheet.getRange(`A2:A`);
-  const values = sheet.getDataRange().getValues();
-  // 検索
-  const textFinder = titleRange.createTextFinder(searchWords).useRegularExpression(true);
-  const targetRanges = textFinder.findAll();
-  return targetRanges.map((r)=>{
-      let obj = {};
-      // valuesはヘッダー行を含まない0オーダー && rowIndexは1オーダーなので
-      const rNum = r.getRowIndex()-1;
-      Logger.log(`${rNum}: ${values[rNum]}`);
-      values[rNum].map((item, index) => {
-        obj[String(header[index])] = String(item);
-      });
-      return obj;
-    });
+    // とくにjsonとか考えなくても文tableHeader2字列のまま取得できた 配列も同じ
+    const searchWords = `(${words.trim().replaceAll(/(　| |\\|\|)+/g, " ").split(" ").join("|")})`;
+    console.log(`search target words: ${searchWords}`);
+    // 検索対象はとりあえずタイトルだけ
+    const titleRange = sheet.getRange(`A2:A`);
+    const values = sheet.getDataRange().getValues();
+    // 検索
+    if(words !== ""){
+        const textFinder = titleRange.createTextFinder(searchWords).useRegularExpression(true);
+        const targetRanges = textFinder.findAll();
+        return targetRanges.map((r)=>{
+            let obj = {};
+            // valuesはヘッダー行を含まない0オーダー && rowIndexは1オーダーなので
+            const rNum = r.getRowIndex()-1;
+            Logger.log(`${rNum}: ${values[rNum]}`);
+            values[rNum].map((item, index) => {
+            obj[String(header[index])] = String(item);
+            });
+            return obj;
+        });
+    }else{        
+        console.log("検索ワード空だったよ～");
+        values.shift();
+        return values.map((row)=>{
+            let obj = {};
+            row.map((item, index) => {
+              obj[String(header[index])] = String(item);
+            });
+            return obj;
+        });
+    }
 }
 
